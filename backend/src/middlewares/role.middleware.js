1f2761a416f805b1c2acabc2aec3f9c955e267d4
joni <codingmylife0@gmail.com>
2026-01-26 22:44:35 +0530

    spelling issu sloved

--- backend/src/middlewares/role.middleware.js
+++ backend/src/middlewares/role.middleware.js
@@ -1,6 +1,5 @@
 const authorizeRoles = (...roles) => {
     return (req, res, next) => {
-
         if (!req.user || !req.user.role) {
             return res.status(403).json({
                 success: false,
