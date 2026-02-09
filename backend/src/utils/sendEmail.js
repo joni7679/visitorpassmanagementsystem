bfb5022f25e34c503ecc65980cf6f8eda2dd0709
joni <codingmylife0@gmail.com>
2026-02-10 01:05:57 +0530

    nodemailer host name change

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
+    host: "gmail",
     port: 587,
     secure: false,
     auth: {
